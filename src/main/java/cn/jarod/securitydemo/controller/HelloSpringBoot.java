f157d0b3d11946341288a53826cfe953177171f6
jarod-jin <kira277@163.com>
2018-06-25 20:50:29 +0800

    代码提交

--- src/main/java/cn/jarod/securitydemo/controller/HelloSpringBoot.java
+++ src/main/java/cn/jarod/securitydemo/controller/HelloSpringBoot.java
@@ -1,16 +1,27 @@
 package cn.jarod.securitydemo.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/hello")
 public class HelloSpringBoot {
 
 
+  @RequestMapping(value={"/say/{code}"})
+  public String say(@PathVariable("code") String str){
+    return "id:"+str;
+  }
+
   @RequestMapping(value={"/say"})
-  public String say(@RequestParam("id") Integer myId){
+  public String say2(@RequestParam("id") String myId){
     return "id:"+myId;
+  }
+
+  //负责处理/index.html的请求
+  @RequestMapping(value = {"/" , "/index.html"})//可配置多个映射路径
+  public String loginPage(){
+    return "login";
   }
 }
--- src/test/java/cn/jarod/securitydemo/controller/HelloSpringBootTest.java
+++ src/test/java/cn/jarod/securitydemo/controller/HelloSpringBootTest.java
@@ -1,5 +1,8 @@
 package cn.jarod.securitydemo.controller;
 
+import org.junit.Test;
+
 public class HelloSpringBootTest {
+
 
 }
